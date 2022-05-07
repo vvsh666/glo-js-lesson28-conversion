d984ebc0bb2a342a6b241d0176cb768d735a25c9
Victor <vvsh1978@mail.ru>
2022-05-07 21:37:05 +0500

    feat: add listener to submit

--- script.js
+++ script.js
@@ -7,28 +7,25 @@ const labelInputs = form.querySelectorAll('.label-inputs')
 const changeBtn = form.querySelector('.change-direction')
 const inputBoxes = form.querySelectorAll('.input-box')
 
-// const getData = async (from, to, amount) => {
-//     const myHeaders = new Headers();
-//     myHeaders.append("apikey", "1RLtjd9b9mwo7XH9kYH630LFaHByCNUW");
+const getData = async (from, to, amount) => {
+    const myHeaders = new Headers();
+    myHeaders.append("apikey", "1RLtjd9b9mwo7XH9kYH630LFaHByCNUW");
 
-//     const requestOptions = {
-//         method: 'GET',
-//         redirect: 'follow',
-//         headers: myHeaders
-//     };
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
 
-//     try {
-//         const targetUrl = `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`
-//         const response = await fetch(targetUrl, requestOptions)
+    try {
+        const targetUrl = `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`
+        const response = await fetch(targetUrl, requestOptions)
 
-//         return await response.json()
-//     } catch (error) {
-//         throw new Error(error)
-//     }
-// }
-
-// getData('usd', 'rub', 100)
-//     .then(data => console.log(data.result))
+        return await response.json()
+    } catch (error) {
+        throw new Error(error)
+    }
+}
 
 // const clearInputs = () => {
 //     inputs.forEach(input => {
@@ -65,9 +62,28 @@ changeBtn.addEventListener('click', (e) => {
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const amount = form.querySelector('input[name="first"]').value
+    let from, to
+    if (!inputBoxes[0].classList.contains('money')) {
+        from = 'rub'
+        if (radios[0].checked === true) {
+            to = 'usd'
+        } else {
+            to = 'eur'
+        }
+    } else {
+        to = 'rub'
+        if (radios[0].checked === true) {
+            from = 'usd'
+        } else {
+            from = 'eur'
+        }
+    }
 
-
-    inputs
+    getData(from, to, amount)
+    .then(data => {
+        inputs[1].value = data.result.toFixed(2)
+    })
+    .catch(error => console.log(error.message))
 })
 
 
